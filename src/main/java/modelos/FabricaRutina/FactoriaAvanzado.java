fd39f7ce21feaa6eda926c0903a606c46a7f9153
Daniel PM <dpm10yt@gmail.com>
2023-01-14 18:45:42 +0100

    factorias rutina

--- src/main/java/modelos/FabricaRutina/FactoriaAvanzado.java
+++ src/main/java/modelos/FabricaRutina/FactoriaAvanzado.java
@@ -1,5 +1,6 @@
 package modelos.FabricaRutina;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,16 +8,23 @@ import java.util.List;
  * @version 1.0
  * @created 06-ene.-2023 17:48:20
  */
-public class FactoriaAvanzado implements FabricaRutina {
+public class FactoriaAvanzado extends FabricaRutina {
 
-	public FactoriaAvanzado(){
+    public Rutina crearEjercicios() {
+        List<Ejercicio> ejerciciosEmpuje = new ArrayList<>();
+        List<Ejercicio> ejerciciosTiron = new ArrayList<>();
+        List<Ejercicio> ejerciciosPierna = new ArrayList<>();
 
-	}
+        for (String nombreEj : listaEjerciciosEmpuje) {
+            ejerciciosEmpuje.add(new Ejercicio(nombreEj, 12, 4, utiles.Enum.Ejercicio.EMPUJE));
+        }
+        for (String nombreEj : listaEjerciciosTiron) {
+            ejerciciosTiron.add(new Ejercicio(nombreEj, 12, 4, utiles.Enum.Ejercicio.TIRON));
+        }
+        for (String nombreEj : listaEjerciciosPierna) {
+            ejerciciosPierna.add(new Ejercicio(nombreEj, 12, 4, utiles.Enum.Ejercicio.PIERNA));
+        }
 
-	public void finalize() throws Throwable {
-
-	}
-	public List<Ejercicio> crearEjercicios(){
-		return null;
-	}
+        return new Rutina(ejerciciosEmpuje, ejerciciosTiron, ejerciciosPierna);
+    }
 }//end FactoriaAvanzado
