39048d385f0840b5e3714b0f2dbaa4e2c1957713
Octavian Parfene <octaparfene1@gmail.com>
2023-01-17 19:44:39 +0100

    Añadido abrir ventanas al puslar sobre botones

--- src/main/java/interfaces/VistasCliente/VistaUsuarios.java
+++ src/main/java/interfaces/VistasCliente/VistaUsuarios.java
@@ -4,6 +4,8 @@
  */
 package interfaces.VistasCliente;
 
+import interfaces.VistasAdmin.ConsultarReservas;
+
 /**
  *
  * @author Octavian
@@ -105,21 +107,30 @@ public class VistaUsuarios extends javax.swing.JFrame {
 
     private void jButtonBusquedaServiciosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonBusquedaServiciosActionPerformed
         // TODO add your handling code here:
+        ConsultarReservas cr = new ConsultarReservas();
+        cr.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButtonBusquedaServiciosActionPerformed
 
     private void jButtonBusquedaEquipoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonBusquedaEquipoActionPerformed
         // TODO add your handling code here:
+        InterfazBuscarEquipo ibeq = new InterfazBuscarEquipo();
+        ibeq.setVisible(true);
+        this.dispose();
+
     }//GEN-LAST:event_jButtonBusquedaEquipoActionPerformed
 
     private void jButtonInterfazGimnasioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonInterfazGimnasioActionPerformed
         // TODO add your handling code here:
+        InterfazGimnasio ig = new InterfazGimnasio();
+        ig.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButtonInterfazGimnasioActionPerformed
 
     private void jButtonCerrarSesionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonCerrarSesionActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButtonCerrarSesionActionPerformed
 
-    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButtonBusquedaEquipo;
